81c517297a2b8a73d277ed9c6c94c717a742771b
Ross Bencina <rossb@audiomulch.com>
2023-07-23 15:10:48 +1000

    change unnecessary f-strings to plain strings

--- prapti/tool/__init__.py
+++ prapti/tool/__init__.py
@@ -49,7 +49,7 @@ def load_config_file(config_path: pathlib.Path, state: ExecutionState) -> bool:
         loads without error.
     """
     if config_path.is_file():
-        state.log.detail("loading-config", f"loading configuration file", config_path)
+        state.log.detail("loading-config", "loading configuration file", config_path)
         try:
             config_file_lines = config_path.read_text(encoding="utf-8").splitlines(keepends=True)
             parse_messages_and_interpret_commands(config_file_lines, config_path, state)
@@ -81,7 +81,7 @@ def default_load_config_files(state: ExecutionState):
 
     # if no config file is present, use fallback config
     if not found_config_file:
-        state.log.detail("loading-fallback-config", f"loading fallback configuration", state.input_file_path)
+        state.log.detail("loading-fallback-config", "loading fallback configuration", state.input_file_path)
         parse_messages_and_interpret_commands(fallback_config_file_data.splitlines(keepends=True), pathlib.Path("<fallback-config>"), state)
 
 def find_final_prompt_message(messages: list[Message]) -> Message|None:
@@ -134,7 +134,7 @@ def main(argv: Sequence[str] | None = None) -> int:
 
     # process input file, generate response
     with open(state.input_file_path, "rt+", encoding="utf-8") as file:
-        state.log.detail("loading-input", f"loading input file", state.input_file_path)
+        state.log.detail("loading-input", "loading input file", state.input_file_path)
         lines = file.readlines()
         # early-out if the input file is effectively empty
         if not lines or all(not line.strip() for line in lines): # if file is effectively empty
