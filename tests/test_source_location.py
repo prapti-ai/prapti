9007d1605bb171e59dfcfc7a19f81dba28a98f22
Ross Bencina <rossb@audiomulch.com>
2023-07-28 19:18:28 +1000

    cleanup test_source_location

--- tests/test_source_location.py
+++ tests/test_source_location.py
@@ -1,16 +1,7 @@
 from pathlib import Path
 from prapti.core.source_location import SourceLocation
 
-from pydantic import BaseModel, ConfigDict
-
-class Foo(BaseModel):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    bar : int = 1
-    #source_loc : SourceLocation|None = None
-
 def test_source_location():
-    foo = Foo()
-
     file_path = Path("test/path")
 
     source_loc = SourceLocation()
@@ -32,4 +23,3 @@ def test_source_location():
     assert source_loc.file_path == file_path
     assert source_loc.line == 42
     assert source_loc.column == 101
-
