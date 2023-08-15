46589582ab0186e25d02df6d127e4cf473d3dbbc
Ross Bencina <rossb@audiomulch.com>
2023-08-15 17:53:39 +1000

    improve tests: factor mock home directory creation into fixtures

--- tests/test_config_files.py
+++ tests/test_config_files.py
@@ -13,20 +13,13 @@ Load a responder, but we don't expect this to load
 % plugins.load prapti.test.test_responder
 % responder.new default prapti.test.test_responder
 """
-def test_no_default_config(tmp_path: pathlib.Path, monkeypatch):
+def test_no_default_config(tmp_path: pathlib.Path, mock_user_home: pathlib.Path, tmp_xdg_config_home: pathlib.Path, monkeypatch):
     """Test that when the --no-default-config flag is specified, no default responder is loaded"""
 
     # set up mock user config.md and .prapticonfig.md in all the places, so we can check that none of them load
 
-    # set up mocked XDG_CONFIG_HOME and write config.md
-    mock_xdg_config_home = tmp_path / "mock_xdg_config_home"
-    mock_xdg_config_home.mkdir()
-    monkeypatch.setenv("XDG_CONFIG_HOME", str(mock_xdg_config_home))
-
-    mock_prapti_config_dir_1 = mock_xdg_config_home / "prapti"
+    mock_prapti_config_dir_1 = tmp_xdg_config_home / "prapti"
     mock_prapti_config_dir_1.mkdir()
-
-    mock_user_home = tmp_path / "mock_user_home"
 
     mock_prapti_config_dir_2 = mock_user_home / ".config" / "prapti"
     mock_prapti_config_dir_2.mkdir(parents=True)
@@ -51,20 +44,8 @@ def test_no_default_config(tmp_path: pathlib.Path, monkeypatch):
     state: ExecutionState = test_exfil["state"]
     assert not hasattr(state.root_config.responders, "default")
 
-def test_fallback_config(tmp_path: pathlib.Path, monkeypatch):
+def test_fallback_config(tmp_path: pathlib.Path, no_user_config, monkeypatch):
     """Test that when no .prapticonfig.md nor user config.md is present, a default responder is loaded (via the fallback configuration)"""
-
-    # clear XDG_CONFIG_HOME, set up mocked user home directory
-    monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
-
-    # leave mock user home empty. i.e. no user config
 
     # minimal md input file
     temp_md_path = tmp_path / "test_fallback_config.md"
@@ -85,18 +66,16 @@ XDG_CONFIG_HOME_CONFIG = """\
 % responder.new default prapti.test.test_responder
 % responders.default.a_string = "Loaded from $XDG_CONFIG_HOME/prapti/config.md"
 """
-def test_load_user_config_from_xdg_config_home(tmp_path: pathlib.Path, monkeypatch):
+def test_load_user_config_from_xdg_config_home(tmp_path: pathlib.Path, mock_user_home: pathlib.Path, tmp_xdg_config_home: pathlib.Path, monkeypatch):
     """Test loading user config from $XDG_CONFIG_HOME/prapti/config.md when present"""
 
     # set up mocked XDG_CONFIG_HOME and write config.md
-    mock_xdg_config_home = tmp_path / "mock_xdg_config_home"
-    mock_xdg_config_home.mkdir()
-    monkeypatch.setenv("XDG_CONFIG_HOME", str(mock_xdg_config_home))
-
-    mock_prapti_config_dir = mock_xdg_config_home / "prapti"
+    mock_prapti_config_dir = tmp_xdg_config_home / "prapti"
     mock_prapti_config_dir.mkdir()
 
     (mock_prapti_config_dir / "config.md").write_text(XDG_CONFIG_HOME_CONFIG, encoding="utf-8")
+
+    # leave mocked user home empty
 
     # minimal md input file
     temp_md_path = tmp_path / "test_load_user_config_from_xdg_config_home.md"
@@ -117,18 +96,11 @@ DEFAULT_XDG_CONFIG_HOME_CONFIG = """\
 % responder.new default prapti.test.test_responder
 % responders.default.a_string = "Loaded from ~/.config/prapti/config.md"
 """
-def test_load_user_config_from_default_xdg_config_home(tmp_path: pathlib.Path, monkeypatch):
+def test_load_user_config_from_default_xdg_config_home(tmp_path: pathlib.Path, mock_user_home: pathlib.Path, monkeypatch):
     """Test loading user config from ~/.config/prapti/config.md when present"""
 
     # clear XDG_CONFIG_HOME, set up mocked user home directory, and write config.md
     monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
 
     mock_default_xdg_config_home = mock_user_home / ".config"
     mock_default_xdg_config_home.mkdir()
@@ -157,18 +129,11 @@ LEGACY_CONFIG_HOME_CONFIG = """\
 % responder.new default prapti.test.test_responder
 % responders.default.a_string = "Loaded from ~/.prapti/config.md"
 """
-def test_load_user_config_from_legacy_config_home(tmp_path: pathlib.Path, monkeypatch):
+def test_load_user_config_from_legacy_config_home(tmp_path: pathlib.Path, mock_user_home: pathlib.Path, monkeypatch):
     """Test loading user config from ~/.prapti/config.md when present"""
 
     # clear XDG_CONFIG_HOME, set up mocked user home directory, and write config.md
     monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
 
     mock_prapti_config_dir = mock_user_home / ".prapti"
     mock_prapti_config_dir.mkdir()
@@ -193,15 +158,11 @@ XDG_CONFIG_HOME_CONFIG_NO_RESPONDER = """\
 % plugins.load prapti.test.test_config
 % plugins.prapti.test.test_config.a_string = "Loaded from $XDG_CONFIG_HOME/prapti/config.md"
 """
-def test_load_user_config_inhibits_fallback_config(tmp_path: pathlib.Path, monkeypatch):
+def test_load_user_config_inhibits_fallback_config(tmp_path: pathlib.Path, tmp_xdg_config_home: pathlib.Path, monkeypatch):
     """Test that when a user config.md is provided, no fallback default responder is loaded"""
 
     # set up mocked XDG_CONFIG_HOME and write config.md
-    mock_xdg_config_home = tmp_path / "mock_xdg_config_home"
-    mock_xdg_config_home.mkdir()
-    monkeypatch.setenv("XDG_CONFIG_HOME", str(mock_xdg_config_home))
-
-    mock_prapti_config_dir = mock_xdg_config_home / "prapti"
+    mock_prapti_config_dir = tmp_xdg_config_home / "prapti"
     mock_prapti_config_dir.mkdir()
 
     (mock_prapti_config_dir / "config.md").write_text(XDG_CONFIG_HOME_CONFIG_NO_RESPONDER, encoding="utf-8")
@@ -225,20 +186,8 @@ PRAPTICONFIG_MD = """\
 % responder.new default prapti.test.test_responder
 % responders.default.a_string = "Loaded from .prapticonfig.md"
 """
-def test_prapticonfig_md(tmp_path: pathlib.Path, monkeypatch):
+def test_prapticonfig_md(tmp_path: pathlib.Path, no_user_config, monkeypatch):
     """Test loading configuration from .prapticonfig.md"""
-
-    # clear XDG_CONFIG_HOME, set up empty mocked user home directory
-    monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
-
-    # leave mock user home empty. i.e. no user config
 
     # .prapticonfig.md
     temp_prapticonfig_md_path = tmp_path / ".prapticonfig.md"
@@ -262,20 +211,8 @@ PRAPTICONFIG_MD_NO_RESPONDER = """\
 % plugins.load prapti.test.test_config
 % plugins.prapti.test.test_config.a_string = "Loaded from .prapticonfig.md"
 """
-def test_load_prapticonfig_md_inhibits_fallback_config(tmp_path: pathlib.Path, monkeypatch):
+def test_load_prapticonfig_md_inhibits_fallback_config(tmp_path: pathlib.Path, no_user_config, monkeypatch):
     """Test that when a .prapticonfig.md is provided, no fallback default responder is loaded"""
-
-    # clear XDG_CONFIG_HOME, set up empty mocked user home directory
-    monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
-
-    # leave mock user home empty. i.e. no user config
 
     # .prapticonfig.md
     temp_prapticonfig_md_path = tmp_path / ".prapticonfig.md"
@@ -306,20 +243,8 @@ LOAD_ORDER_CHILD_PRAPTICONFIG_MD = """\
 This `.prapticonfig.md` file should load second, and hence the following field value should be visible to the test:
 % responders.default.a_string = "Loaded from ./.prapticonfig.md"
 """
-def test_prapticonfig_md_load_order(tmp_path: pathlib.Path, monkeypatch):
+def test_prapticonfig_md_load_order(tmp_path: pathlib.Path, no_user_config, monkeypatch):
     """Test that .prapticonfig.md files are loaded starting from the directory closest to the root, and working towards the directory containing the input file."""
-
-    # clear XDG_CONFIG_HOME, set up empty mocked user home directory
-    monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
-
-    # leave mock user home empty. i.e. no user config
 
     parent_path = tmp_path
     child_path = tmp_path / "child"
@@ -370,20 +295,8 @@ CONFIG_ROOT_CHILD_PRAPTICONFIG_MD_2 = """\
 % responders.default.a_string = "Loaded from ./.prapticonfig.md"
 """
 @pytest.mark.parametrize("root_prapticonfig_md", [CONFIG_ROOT_CHILD_PRAPTICONFIG_MD_1, CONFIG_ROOT_CHILD_PRAPTICONFIG_MD_2])
-def test_prapticonfig_md_config_root(tmp_path: pathlib.Path, monkeypatch, root_prapticonfig_md):
+def test_prapticonfig_md_config_root(tmp_path: pathlib.Path, no_user_config, monkeypatch, root_prapticonfig_md):
     """Test that .prapticonfig.md file search terminates at the closest directory to the input file containing % config_root = true."""
-
-    # clear XDG_CONFIG_HOME, set up empty mocked user home directory
-    monkeypatch.delenv("XDG_CONFIG_HOME", raising=False)
-
-    mock_user_home = tmp_path / "mock_user_home"
-    mock_user_home.mkdir()
-    monkeypatch.setenv("HOME", str(mock_user_home))
-    def mock_home_func():
-        return mock_user_home
-    monkeypatch.setattr(pathlib.Path, "home", mock_home_func)
-
-    # leave mock user home empty. i.e. no user config
 
     parent_path = tmp_path
     child_path = tmp_path / "child"
